359c08d1928a0662d05269793683a598ad8f1af8
l14d35 <kujawa.dp@gmail.com>
2020-03-11 15:48:31 +0100

    initial project structure

---
+++ src/main/java/com/metabug/MetaBugApplication.java
@@ -0,0 +1,12 @@
+package com.metabug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaBugApplication.class, args);
+    }
+}
---
+++ src/test/java/com/metabug/MetaBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metabug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

944b8a70a32591a3ad5558a209b1674d2e2d8fab
l14d35 <kujawa.dp@gmail.com>
2020-05-03 17:22:13 +0200

    Fixed homepage navbar

--- src/main/java/com/metabug/web/controller/HomeController.java
+++ src/main/java/com/metabug/web/controller/HomeController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
     @GetMapping(value = {"/home"})
-    public ModelAndView home() {
+    public ModelAndView home(final Principal principal) {
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("home");
+        modelAndView.addObject("username", principal.getName());
         return modelAndView;
     }
 }
